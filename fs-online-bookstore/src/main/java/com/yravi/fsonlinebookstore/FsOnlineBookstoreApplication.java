c4d6ef046bb8ca2a7b0898eef831c94667d66702
yraviui <yravi.ui@gmail.com>
2020-06-17 05:25:01 -0700

    Initial commit. Created spring boot backend project with application properties

---
+++ fs-online-bookstore/src/main/java/com/yravi/fsonlinebookstore/FsOnlineBookstoreApplication.java
@@ -0,0 +1,13 @@
+package com.yravi.fsonlinebookstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FsOnlineBookstoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FsOnlineBookstoreApplication.class, args);
+	}
+
+}
---
+++ fs-online-bookstore/src/test/java/com/yravi/fsonlinebookstore/FsOnlineBookstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yravi.fsonlinebookstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FsOnlineBookstoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
